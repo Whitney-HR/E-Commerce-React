59d81785532798ceb5cc2912ee6bb82efb9f97d2
Nicholas Ledan <Nickledan5@gmail.com>
2021-07-22 14:18:44 -0400

    init

--- server/index.js
+++ server/index.js
@@ -1,1 +1,61 @@
+let express = require('express');
+let path = require('path');
+let controller = require('./controllers.js');
 
+
+let app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
+
+//ROUTES TO BE ADDED FOR GET/POST? UPDATE/DElETE
+
+
+// app.get('atelier/api', controller.review.get)
+
+
+/*
+GET /products
+GET /products/:product_id
+GET /products/:product_id/styles
+GET /products/:product_id/related
+
+GET/reviews/
+GET/reviews/meta
+POST/reviews
+PUT /reviews/:review_id/helpful
+PUT /reviews/:review_id/report
+
+GET /qa/questions
+GET /qa/questions/:question_id/answers
+POST /qa/questions
+POST /qa/questions/:question_id/answers
+PUT /qa/questions/:question_id/helpful
+PUT /qa/questions/:question_id/report
+
+GET /cart
+POST /cart
+
+POST /interactions
+*/
+
+/**
+ * var server = app.listen(8080, function () {
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Listening on: ", host, port)
+})
+
+const port = 3010;
+
+app.listen(port, () => {
+  console.log('Success! Server Initialized');
+})
+ */
+
+const port =  3737;
+
+app.listen(port, () => {
+  console.log(`Listening on Port: ${port}`);
+})
