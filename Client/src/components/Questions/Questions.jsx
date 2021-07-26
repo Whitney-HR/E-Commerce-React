7ca3396b48c9e813df6288e06325bd5dfd9be1cb
Nicholas Ledan <Nickledan5@gmail.com>
2021-07-26 14:50:08 -0400

    Added Buttons

--- Client/src/components/Questions/Questions.jsx
+++ Client/src/components/Questions/Questions.jsx
@@ -3,23 +3,30 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 import App from '../App.jsx';
 import SearchBar from './SearchBar.jsx';
+import AddQuestion from './AddQuestion.jsx';
+import MoreAnsweredQuestion from './MoreAnsweredQuestions.jsx';
 
-// function Questions() {
+export default function Questions() {
+  return (
+    <div>
+      <SearchBar />
+      <MoreAnsweredQuestion /> <AddQuestion />
+    </div>
+  )
+};
 
+
+// class Questions extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <SearchBar />
+//       </div>
+//     )
+//   }
 // }
-class Questions extends React.Component {
-  constructor(props) {
-    super(props);
-
-  }
-
-  render() {
-    return (
-      <div>
-        <SearchBar />
-      </div>
-    )
-  }
-}
-
-export default Questions;
