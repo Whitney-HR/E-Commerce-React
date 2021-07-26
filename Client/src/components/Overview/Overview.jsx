b43b66cf1bcaa4c57f27c177aa2881ad5d0e1bf8
Santiago Vera Nicola <sveranicola12@gmail.com>
2021-07-26 16:34:01 -0400

    rendering item details

--- Client/src/components/Overview/Overview.jsx
+++ Client/src/components/Overview/Overview.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import token from '/Client/src/env/config.js'
+import randomNumber from '../Shared/randomNumberInator.js'
 
 var Overview = () => {
-  const [products, productsUpdate] = useState([])
+  const [products, productsUpdate] = useState([]);
+
   useEffect (() => {
     Axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products',   { headers: {  Authorization: token } })
       .then(data => {
@@ -11,16 +13,25 @@ var Overview = () => {
       });
   }, []);
 
+  var numRand = randomNumber(0, products.length);
 
+  var item = {
+    name: 'Loading please wait'
+  };
 
+  if (products.length) {
+    item = products[numRand];
+  }
 
   return (
     <div>
-      <ul>
-        {products.map(item => (
-          <li>{item.name}</li>
-        ))}
-      </ul>
+      <div>{item.category}</div>
+      <div><h3>{item.name}</h3></div>
+      <div>{item.default_price}</div>
+      <div>
+        <p>{item.slogan}</p>
+        <p>{item.description}</p>
+      </div>
     </div>
   )
 };
