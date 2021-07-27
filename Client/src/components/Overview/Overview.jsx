cb5008e785111f66314683365c5fb40b4711d1d0
Santiago Vera Nicola <sveranicola12@gmail.com>
2021-07-27 13:00:48 -0400

    getting data from axios at product id

--- Client/src/components/Overview/Overview.jsx
+++ Client/src/components/Overview/Overview.jsx
@@ -3,29 +3,22 @@ import Axios from 'axios';
 import token from '/Client/src/env/config.js'
 import randomNumber from '../Shared/randomNumberInator.js'
 
-var Overview = () => {
-  // const [products, productsUpdate] = useState([]);
+var Overview = (props) => {
+  const [product, productUpdate] = useState({});
 
-  // useEffect (() => {
-  //   Axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products',   { headers: {  Authorization: token } })
-  //     .then(data => {
-  //       productsUpdate(data.data);
-  //     });
-  // }, []);
+  useEffect (() => {
+    Axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/${props.id}`,   { headers: {  Authorization: token } })
+      .then(data => {
+        productUpdate(data.data);
+      });
+  }, [props.id]);
 
-  // var numRand = randomNumber(0, products.length);
-
-  // var item = {
-  //   name: 'Loading please wait'
-  // };
-
-  // if (products.length) {
-  //   item = products[numRand];
-  // }
 
   return (
     <div>
-
+      <h1>
+        {product.name}
+      </h1>
     </div>
 
   )
--- Client/src/components/app.jsx
+++ Client/src/components/app.jsx
@@ -32,15 +32,15 @@ var App = () => {
   return (
     <div>
       <h1>E-COMMERCE</h1>
-      <div>{item.category}</div>
+      {/* <div>{item.category}</div>
       <div><h3>{item.name}</h3></div>
       <div>{item.default_price}</div>
       <div>
         <p>{item.slogan}</p>
         <p>{item.description}</p>
-      </div>
-      {/* <Overview id={item.id}/>
-      <Questions id={item.id}/>
+      </div> */}
+      <Overview id={item.id}/>
+      {/* <Questions id={item.id}/>
       <Reviews id={item.id}/> */}
     </div>
 
