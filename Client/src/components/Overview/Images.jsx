16ed549059193b5236937d79e9c98741ccd740ff
Santiago Vera Nicola <sveranicola12@gmail.com>
2021-07-29 00:12:06 -0400

    added pictures to choose from

--- Client/src/components/Overview/Images.jsx
+++ Client/src/components/Overview/Images.jsx
@@ -1,13 +1,40 @@
 import React, { useState, useEffect } from 'react';
 
 var Images = (props) => {
-  //const [] = useState({})
-  console.log(props.chosenStyle);
+  const [bigPicture, updateBigPicture] = useState({})
+  ///console.log(props.chosenStyle);
+
+  const pictureSelect = {
+    border: '2px solid black',
+    height: '80px',
+    width: '80px'
+  }
+  const pictureSelected = {
+    border: '2px solid black',
+    height: '700px',
+    // width: '700px'
+  }
+
+  var picsInputCss = pictureSelect;
+
+  var changeBigPic = (negatives) => {
+    updateBigPicture(negatives);
+  }
+
+
   return (
     <div>
-      {props.chosenStyle.photos.map((currentPicture, index) => {
-        <img src={currentPicture.thumbnail_url} key={index}></img>
-      })}
+      <div>
+        {props.chosenStyle.photos.map((currentPicture, index) => {
+
+          return (
+            <img style={pictureSelect} src={currentPicture.thumbnail_url} key={index} onClick={() => {changeBigPic(currentPicture) }}></img>
+          )
+        })}
+      </div>
+      <div>
+        <img style={pictureSelected} src={bigPicture.url} ></img>
+      </div>
     </div>
   )
 };
