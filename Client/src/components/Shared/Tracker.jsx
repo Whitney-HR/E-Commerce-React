cfc6d8417822a2134dfb5892c42d54675759b144
Santiago Vera Nicola <sveranicola12@gmail.com>
2021-08-03 15:29:16 -0400

    added stars and tracker

---
+++ Client/src/components/Shared/Tracker.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect, useLayoutEffect } from 'react';
+import token from '/Client/src/env/config.js';
+import axios from 'axios';
+
+var Tracker = (element, widget) => {
+  var today = new Date ();
+  var stringDay = JSON.stringify(today);
+
+  var body = {"element": element , "widget": widget, "time": stringDay}
+
+  axios.post (`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/interactions`, body , { headers: {  Authorization: token } })
+  .then(data => {
+    alert('GET PWND!');
+  })
+  .catch(err => {
+    console.log(err)
+  });
+
+
+}
+
+export default Tracker;
